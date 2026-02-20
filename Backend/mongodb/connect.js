390c26a54aeb6dc0239f9da906ac896da3828b93
Raheemak <ahsanasif89867@gmail.com>
2026-02-20 12:45:16 +0500

    Error fixing

--- Backend/mongodb/connect.js
+++ Backend/mongodb/connect.js
@@ -4,8 +4,6 @@ const connectDB = async (url) => {
   try {
     mongoose.set("strictQuery", true);
     await mongoose.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log("MongoDB Connected ");
   } catch (err) {
